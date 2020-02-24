dc34ecfbc2aec9dc3afb66366e1ef5538471cb02
oleg.nikitin <oleg.nikitin@AMS-GYM-EDV1-06.gym.ams.local>
2020-02-24 17:58:44 +0100

    Absturzgefahr

---
+++ src/GUIIntro/MyfirstWindow.java
@@ -0,0 +1,58 @@
+package GUIIntro;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+
+public class MyfirstWindow {
+
+	protected Shell shell;
+	private Text Vorname;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			MyfirstWindow window = new MyfirstWindow();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+		
+		Vorname = new Text(shell, SWT.BORDER);
+		Vorname.setBounds(193, 99, 76, 21);
+		
+		Button btnVorname = new Button(shell, SWT.NONE);
+		btnVorname.setBounds(94, 99, 75, 25);
+		btnVorname.setText("Vorname");
+
+	}
+}
